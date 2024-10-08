eb6b65665b9d1a84afe9fdd2a6cd512f33480132
shamim <srecon@yandex.ru>
2024-10-08 12:17:54 +0300

    add author

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -115,7 +115,7 @@ export default async function BlogPage() {
             </main>
             <footer className="bg-white shadow mt-12">
                 <div className="container mx-auto px-4 py-6 text-center text-gray-600">
-                    <p>&copy; 2024 Shamim Bhuyan and Timur Isachenko. All rights reserved.</p>
+                    <p>&copy; 2024 Shamim Bhuiyan and Timur Isachenko. All rights reserved.</p>
                 </div>
             </footer>
         </div>
